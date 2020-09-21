a83561c990ce731e00e2d6c37a4c1d260ba01949
Juan Matus <juanmatusg16@gmail.com>
2020-09-21 11:42:40 -0600

    Added Styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './assets/scss/styles.css'
+import Form from './components/Form'
+import TodoList from './components/TodoList'
 
 function App() {
+
+  // State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  //Run once when the app starts
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true))
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <div className="App"> 
+    <header>
+      <h1>Juan's Todo List </h1>
+    </header>
+    <Form 
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      setStatus={setStatus}      
+    />
+    <TodoList 
+      filteredTodos={filteredTodos} 
+      setTodos={setTodos} 
+      todos={todos} 
+      />
     </div>
   );
 }
-
 export default App;
